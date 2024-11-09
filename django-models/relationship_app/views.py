ea75e21abf7699d3e06c74100293f559d740291a
Chuksugo <rhemekas.inc@gmail.com>
2024-11-09 21:57:53 +0100

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,15 +1,15 @@
+# relationship_app/views.py
 from django.shortcuts import render
-from django.apps import apps
+from django.views.generic.detail import DetailView  # Import DetailView
+from .models import Book, Library
 
-# Using apps.get_model to load Library dynamically
-Library = apps.get_model('relationship_app', 'Library')
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render with template
 
-def list_books_in_library(request, library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    return render(request, 'relationship_app/book_list.html', {'library': library, 'books': books})
-
-def get_librarian_for_library(request, library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = library.librarian
-    return render(request, 'relationship_app/librarian_detail.html', {'library': library, 'librarian': librarian})
+# Class-based view to display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
