a64cd5aa4959cb33bcf49fba946944d312193347
Chuksugo <rhemekas.inc@gmail.com>
2024-11-09 21:55:34 +0100

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.apps import apps
 
+# Using apps.get_model to load Library dynamically
+Library = apps.get_model('relationship_app', 'Library')
+
 def list_books_in_library(request, library_name):
-    # Load Library model dynamically
-    Library = apps.get_model('relationship_app', 'Library')
-    Book = apps.get_model('relationship_app', 'Book')
-    
-    library = Library.objects.filter(name=library_name).first()
-    books = Book.objects.filter(libraries=library) if library else []
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
     return render(request, 'relationship_app/book_list.html', {'library': library, 'books': books})
 
 def get_librarian_for_library(request, library_name):
-    Library = apps.get_model('relationship_app', 'Library')
-    Librarian = apps.get_model('relationship_app', 'Librarian')
-    
-    library = Library.objects.filter(name=library_name).first()
-    librarian = Librarian.objects.filter(library=library).first() if library else None
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
     return render(request, 'relationship_app/librarian_detail.html', {'library': library, 'librarian': librarian})
