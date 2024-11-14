04ca2b4484d4c4e82a544f9cbd0b4a6384a3e865
Chuksugo <rhemekas.inc@gmail.com>
2024-11-14 20:19:22 +0100

    Implementing Custom Permissions in Django/M

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -125,9 +125,9 @@ def member_view(request):
 
 # relationship_app/views.py
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
@@ -135,30 +135,31 @@ def add_book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to a list of books or desired page
+            return redirect('book_list')  # Redirect to book list after saving
     else:
         form = BookForm()
     return render(request, 'relationship_app/book_form.html', {'form': form})
 
 @permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def edit_book(request, id):
+    book = get_object_or_404(Book, id=id)
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('book_list')  # Redirect to book list after editing
     else:
         form = BookForm(instance=book)
     return render(request, 'relationship_app/book_form.html', {'form': form})
 
 @permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def delete_book(request, id):
+    book = get_object_or_404(Book, id=id)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')
+        return redirect('book_list')  # Redirect to book list after deletion
     return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
+
 
 
 from .forms import BookForm
