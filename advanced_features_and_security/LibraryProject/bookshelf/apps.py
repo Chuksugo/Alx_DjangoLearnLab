c61b1040fa15bdd19b86eedcd0a1bb62a7084c9f
Chuksugo <rhemekas.inc@gmail.com>
2024-11-17 10:10:29 +0100

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -4,3 +4,33 @@ from django.apps import AppConfig
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+
+from django.apps import AppConfig
+from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+class RelationshipAppConfig(AppConfig):
+    name = 'relationship_app'
+
+    def ready(self):
+        # Assign permissions to groups after migration
+        post_migrate.connect(create_groups_permissions, sender=self)
+
+def create_groups_permissions(sender, **kwargs):
+    # Create or get groups
+    viewer_group, created = Group.objects.get_or_create(name='Viewers')
+    editor_group, created = Group.objects.get_or_create(name='Editors')
+    admin_group, created = Group.objects.get_or_create(name='Admins')
+
+    # Get permissions
+    can_view = Permission.objects.get(codename='can_view')
+    can_create = Permission.objects.get(codename='can_create')
+    can_edit = Permission.objects.get(codename='can_edit')
+    can_delete = Permission.objects.get(codename='can_delete')
+
+    # Assign permissions to groups
+    viewer_group.permissions.add(can_view)
+    editor_group.permissions.add(can_view, can_create, can_edit)
+    admin_group.permissions.add(can_view, can_create, can_edit, can_delete)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        # Logic for creating a new book
+        pass
+    return render(request, 'create_book.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        # Logic for editing the book
+        pass
+    return render(request, 'edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        # Logic for deleting the book
+        book.delete()
+    return render(request, 'delete_book.html', {'book': book})
