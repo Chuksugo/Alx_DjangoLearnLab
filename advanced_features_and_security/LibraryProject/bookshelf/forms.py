1b2795d00ebf378983d7efb8939ffec6edca2071
Chuksugo <rhemekas.inc@gmail.com>
2024-11-17 11:22:23 +0100

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
-class BookForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    author = forms.CharField(max_length=100)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label='Your Name')
+
+    # You can add custom validation if needed
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name.isalpha():
+            raise forms.ValidationError("Name should only contain alphabets.")
+        return name
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -42,3 +42,21 @@ def book_view(request):
         if form.is_valid():
             # Process the valid data
             pass
+
+
+
+ from django.shortcuts import render
+from .forms import ExampleForm
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            name = form.cleaned_data['name']
+            return render(request, 'bookshelf/form_example.html', {'form': form, 'name': name})
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+           
