973ce75c922c883aa865059c1d7f41c0959b1c79
Chuksugo <rhemekas.inc@gmail.com>
2024-12-10 15:48:34 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser  # Import the CustomUser model
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
