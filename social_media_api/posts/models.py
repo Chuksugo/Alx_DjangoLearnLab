15aeb792fc12217824892f1d6824d644a4cd1b62
Chuksugo <rhemekas.inc@gmail.com>
2024-12-11 12:50:01 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -7,9 +7,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True, blank=True)  # Add this field
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
